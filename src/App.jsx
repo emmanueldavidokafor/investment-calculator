40f254a4a5bc5ef6368a7ac48fb0999bd6117870
EmmaOkafor <okaforifebuche26@gmail.com>
2024-07-04 20:52:28 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+function handleChange(inputIdentifier, newValue) {
+  setUserInput(prevUserInput => {
+      return {
+          ...prevUserInput,
+          [inputIdentifier]: +newValue,
+      };
+  });
+}
+
+  const inputIsVlid = userInput.duration >= 1;
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputIsVlid && (
+        <p className="center">Please enter a duration greater than 0.</p>
+      )}
+      {inputIsVlid && <Results input={userInput}/>}
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from "../assets/investment-calculator-logo.png";
+
+export default function () {
+  return (
+    <header id='header'>
+        <img src={logo} />
+        <h1>Investment Calculator</h1>
+    </header>
+  )
+}
